35d793760f1f9663431bff5bc69c7c9b2f9d218b
LuisA <86628@alumnos.utleon.edu.mx>
2026-09-18 20:55:21 -0600

    ngModel 18/09/2026

--- src/app/heroes/heroes-list/heroes-list.ts
+++ src/app/heroes/heroes-list/heroes-list.ts
@@ -1,9 +1,54 @@
 import { Component } from '@angular/core';
-
+ 
 @Component({
   selector: 'app-heroes-list',
   standalone: false,
   styleUrl: './heroes-list.css',
   templateUrl: './heroes-list.html',
 })
-export class HeroesList {}
+export class HeroesList {
+imagenWidth:number=40;
+imagenMargin:number=2;
+muestraImage:boolean=true;
+listFilter:string='';
+ 
+showImage():void{
+  this.muestraImage=!this.muestraImage;
+}
+ 
+  heroes:any[]=[
+     {
+      imagen:"https://dragonball-api.com/characters/gohan.webp",
+      nombre:"Son Gohan",
+      descripcion:'Guerrero z',
+      race:'Hibrido Saiyan-human',
+      ki:'10000'
+    },
+    {
+      imagen:"https://dragonball-api.com/characters/goku_normal.webp",
+      nombre:"Son Goku",
+      descripcion:'Guerrero z',
+      race:'Saiyan',
+      ki:'10000'
+    },
+    {
+      imagen:"https://dragonball-api.com/characters/celula.webp",
+      nombre:"Cell",
+      descripcion:'Villano',
+      race:'Androide',
+      ki:'10000'
+    },
+    {
+      imagen:"https://dragonball-api.com/characters/Beerus_DBS_Broly_Artwork.webp",
+      nombre:"Bills",
+      descripcion:'Antes de la creacion viene la destruccion',
+      race:'Dios de la Destruccion',
+      ki:'10000'
+    }
+ 
+  ]
+ 
+ 
+}
+ 
+ 
